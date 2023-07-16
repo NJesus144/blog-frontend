a2e5fa5922029f3c6c104f01a278b5c768a0e659
NJesus144 <nalbert-edm@outlook.com>
2023-07-16 20:01:51 -0300

    alterado componente postID para otimizar a resposta

--- src/pages/PostId/PostId.jsx
+++ src/pages/PostId/PostId.jsx
@@ -1,8 +1,20 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { ContainerPostId } from "../../components/ContainerPostId/ContainerPostId";
+import { CommentSection } from "../../components/CommentSection/CommentSection";
+import { CreatePostComment } from "../../components/CreatePostComment/CreatePostComment";
 import { api } from "../../services/api/api";
 import { getPostById } from "../../services/postsServices";
+import { configToken } from "../../services/token";
+
+import {
+  Container,
+  ContainerMainSection,
+  Description,
+  DivDescription,
+  MainTitle,
+  StyleParagraph,
+  StyledImage,
+} from "./style";
 
 export const PostId = () => {
   const [postId, setPostId] = useState([]);
@@ -11,14 +23,48 @@ export const PostId = () => {
   useEffect(() => {
     const tokenUser = localStorage.getItem("@Auth:token");
     api.defaults.headers.Authorization = `Bearer ${tokenUser}`;
+    try {
+      const fetchPostId = async () => {
+        const response = await getPostById(id, configToken);
+        setPostId(response.data);
+      };
 
-    const fetchPostId = async () => {
-      const response = await getPostById(id);
-      setPostId(response.data);
-    };
-
-    fetchPostId();
+      fetchPostId();
+    } catch (err) {
+      console.log(err.message);
+    }
   });
 
-  return <ContainerPostId post={postId} />;
+  return (
+    <>
+      {postId && (
+        <ContainerMainSection>
+          <Container>
+            <MainTitle>{postId?.title}</MainTitle>
+            <StyleParagraph>
+              <span>Por </span>
+              {postId?.username}
+            </StyleParagraph>
+            <StyledImage image={postId?.banner} />
+
+            <DivDescription>
+              <Description>{postId?.text}</Description>
+            </DivDescription>
+          </Container>
+
+          <CreatePostComment post={postId} />
+          {postId.comments?.length > 0 &&
+            postId.comments.map(commentBlog => (
+              <CommentSection
+                key={commentBlog.idComment}
+                post={postId}
+                userId={commentBlog.userId}
+                idComment={commentBlog.idComment}
+                text={commentBlog.comment}
+              />
+            ))}
+        </ContainerMainSection>
+      )}
+    </>
+  );
 };
